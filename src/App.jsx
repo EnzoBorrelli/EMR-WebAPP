c0666255967894806786659db7b4359e0adc8605
EnderDEV <enzoborrellidev@gmail.com>
2023-04-20 16:12:03 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './components/LandingMenu/LandingMenu'
+import LandingMenu from './components/LandingMenu/LandingMenu';
+
+function App() {
+
+
+  return (
+    <div className="App">
+    
+      <LandingMenu/>
+    </div>
+  )
+}
+
+export default App
