0be14efaacb504a7aa7ce3c49feb56f291c3273d
EnderDEV <enzoborrellidev@gmail.com>
2023-05-22 09:35:33 -0300

    changes

--- src/components/LandingMenu/LandingMenu.jsx
+++ src/components/LandingMenu/LandingMenu.jsx
@@ -5,12 +5,13 @@ import "./LandingMenu.css";
 import logo from "./logo.png";
 import UserNav from "../User/UserNav/UserNav";
 import { useAuth0 } from "@auth0/auth0-react";
+import ShoppingCart from "../User/ShoppingCart/ShoppingCart";
 
 const LandingMenu = () => {
   const [menu, setMenu] = useState(false);
   const [UserToggle, setUserToggle] = useState(false);
   const { texts, handleLanguage } = useContext(LanguageContext);
-  const { user, isAuthenticated} = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
 
   const toggleMenu = () => {
     setMenu(!menu);
@@ -86,9 +87,7 @@ const LandingMenu = () => {
             </NavLink>
           </div>
         </div>
-        <div
-          className="Container-sub--b"
-        >
+        <div className="Container-sub--b">
           <div className="Container-sub--nav">
             <button onClick={toggleMenu} className="LandingMenu-Dropbutton">
               <svg
@@ -165,19 +164,21 @@ const LandingMenu = () => {
               </button>
               <UserNav Toggle={UserToggle} />
             </div>
-            <button className="LandingMenu-UserBtn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="LandingMenu-svg LandingMenu-svg--user"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-              </svg>
-            </button>
-
+            <div className="Container-UserNav">
+              <button className="LandingMenu-UserBtn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="LandingMenu-svg LandingMenu-svg--user"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
+              </button>
+              <ShoppingCart></ShoppingCart>
+            </div>
             <NavLink to="/Contact" className="LandingMenu-a Contact-Button">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
---
+++ src/components/Sections/Catalogue/Product/ProductItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductItem.css'
+
+const ProductItem = () => {
+    return (
+      <div>
+      ProductItem
+      </div>
+    );
+  };
+  
+  export default ProductItem;
